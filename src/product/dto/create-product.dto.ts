0fd7966039832e4c27cfde79290d70a5240dcc18
Erzan12 <do.earljan@gmail.com>
2025-06-10 09:50:37 +0800

    added validation to products

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,6 +1,18 @@
+import { IsString, IsOptional, IsNumber, Min, IsPositive, } from 'class-validator'
+
 export class CreateProductDto {
+  @IsString()
   name: string;
+
+  @IsOptional()
+  @IsString()
   description?: string;
+
+  @IsNumber()
+  @IsPositive()
   price: number;
+
+  @IsNumber()
+  @Min(0)
   quantity: number;
 }
