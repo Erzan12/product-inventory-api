2bb2534619c1d3fb6ba0139eb59b2cf99fa28274
Erzan12 <do.earljan@gmail.com>
2025-06-17 09:46:21 +0800

    added invoice pdf copy feature

--- src/invoice/invoice.controller.ts
+++ src/invoice/invoice.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Res } from '@nestjs/common';
 import { InvoiceService } from './invoice.service';
+import { Response } from 'express';
 
 @Controller('api/invoice')
 export class InvoiceController {
     constructor(private readonly invoiceService: InvoiceService) {}
 
+    //Plain Invoice Receipt
     @Get(':orderId')
     getInvoice(@Param('orderId', ParseIntPipe) orderId: number) {
-        return this.invoiceService.generateInvoice(orderId);
+        return this.invoiceService.generateBasicInvoice(orderId);
+    }
+
+    //PDF Invoice Receipt
+    @Get(':orderId/pdf')
+    generatePdf(@Param('orderId') orderId: number, @Res() res: Response) {
+        return this.invoiceService.generatePDFInvoice(+orderId, res)
     }
 }
